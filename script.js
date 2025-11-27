651213be5e5214c0e3efeecc96c786b1f522e8c8
VerrisisJ <h24verjo@du.se>
2025-11-27 13:06:33 +0100

    Update script.js
    Skrivit in vart variablerna ska in

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Variabel 1 
+
+
+// Variabel 2 
+ 
+
+// Variabel 3 
 const button = document.getElementById("button") //Hittar HTML element som har id=button och sparar som en variabel som heter button
 button.addEventListener("click", function (e) { // Lyssnar efter funktion när knappen klickas, "click" = eventet vi lyssnar efter och e {...} är funktionen som ska köras. e står för event
     console.log("knappen klickades");
@@ -5,3 +12,6 @@ button.addEventListener("click", function (e) { // Lyssnar efter funktion när k
 
 const outputDiv = document.getElementById("div-element");
 console.log(outputDiv); // TA BORT DETTA SENARE! Är endast för att testa om Div är kopplad från HTML filen
+
+// Variabel 4
+ 
