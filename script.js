af090898993681a3b1aa469362223d73a8f8491a
VerrisisJ <h24verjo@du.se>
2025-11-27 13:02:02 +0100

    Update script.js
    Julia uppdaterat script-filen

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("button") //Hittar HTML element som har id=button och sparar som en variabel som heter button
+button.addEventListener("click", function (e) { // Lyssnar efter funktion när knappen klickas, "click" = eventet vi lyssnar efter och e {...} är funktionen som ska köras. e står för event
+    console.log("knappen klickades");
+});
+
+const outputDiv = document.getElementById("div-element");
+console.log(outputDiv); // TA BORT DETTA SENARE! Är endast för att testa om Div är kopplad från HTML filen
