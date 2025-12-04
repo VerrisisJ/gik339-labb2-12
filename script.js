bdaf74b0da61a7059fbd99ec90e32a236f5836fc
VerrisisJ <h24verjo@du.se>
2025-12-04 11:05:59 +0100

    Update script.js
    Uppdaterade eventlyssnare

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ const textFields = document.getElementsByClassName("textfield");
 
 // Variabel 3 
 const button = document.getElementById("button") //Hittar HTML element som har id=button och sparar som en variabel som heter button
-button.addEventListener("click", function (e) { // Lyssnar efter funktion när knappen klickas, "click" = eventet vi lyssnar efter och e {...} är funktionen som ska köras. e står för event
-    console.log("knappen klickades");
-});
 
 // Variabel 4
 const outputDiv = document.querySelector("#divElement");
@@ -29,10 +26,14 @@ function handleDiv(e) {
 
 //Uppgift 6
 
-//textFields
-//handleDiv
-
 checkbox.addEventListener("change", function (e) {    // Lyssnar när checkboxen ändras
     const colorChange = textFields[0].value;          // Hämtar värdet från första textfältet som är "color"
     outputDiv.style.backgroundColor = colorChange;    // Ändrar bakgrundsfärgen på div elementet
+});
+
+textFields[0].addEventListener("input", handleDiv); //Color textfield
+textFields[1].addEventListener("input", handleDiv); //content textfield
+
+button.addEventListener("click", function () { 
+    outputDiv.remove();
 });
